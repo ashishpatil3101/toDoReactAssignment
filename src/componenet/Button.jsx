eb8cdfb9343bf29cf7e57bd1df8987f312bf925e
Vaibhav Patil <ashishpatil3101@gmail.com>
2024-05-09 16:55:06 +0530

    todo row component added which is single row for to do content

--- src/componenet/Button.jsx
+++ src/componenet/Button.jsx
@@ -3,8 +3,7 @@
 
 const Button = ({ onClick, variant, children }) => {
     const buttonClasses = `px-4 py-2 ${variant === 'big' ? 'text-lg' : 'text-sm'} bg-green-500 text-white rounded-md`;
-  
-  return (
+    return (
     <button className={buttonClasses} onClick={onClick}>
       {children}
     </button>
---
+++ src/componenet/ToDoRow.jsx
@@ -0,0 +1,28 @@
+import {useState} from 'react'
+import Button from  './Button';
+import Checkbox from './CheckBox';
+
+const TodoRow = ({ todo, onDelete, onComplete }) => {
+    const [isHovered, setIsHovered] = useState(false);
+  
+    return (
+      <div
+        className="flex justify-between items-center py-2 border-b border-gray-200 hover:bg-gray-100 mb-10"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <Checkbox
+          label={todo.text}
+          isChecked={todo.completed}
+          onChange={() => onComplete(todo.id)}
+        />
+        {isHovered && (
+          <Button variant="small" onClick={() => onDelete(todo.id)}>
+           <i className="ri-close-line"></i>
+          </Button>
+        )}
+      </div>
+    );
+  };
+
+  export default TodoRow;
