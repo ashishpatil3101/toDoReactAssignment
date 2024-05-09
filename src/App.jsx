0f945035c02adefc622790f382eb23b93efa3990
Vaibhav Patil <ashishpatil3101@gmail.com>
2024-05-09 16:08:30 +0530

    started the project and cleanup of predefined code

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      heyecmcc
+      </div>
+    
+    </>
+  )
+}
+
+export default App
