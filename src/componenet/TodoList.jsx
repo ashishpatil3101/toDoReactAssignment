b48d77c9b809ee363cdeb794ac36ad24ff0b2eab
Vaibhav Patil <ashishpatil3101@gmail.com>
2024-05-09 18:04:59 +0530

    app is working fine

--- src/componenet/TodoList.jsx
+++ src/componenet/TodoList.jsx
@@ -29,38 +29,36 @@ const TodoList = () => {
   };
 
   return (
-    // max-w-md mx-auto
-    <div className="w-full h-full mt-8 bg-gray-100 rounded-md">
-      <h1 className="text-2xl font-bold mb-4">Todo List</h1>
-      <div>
-
-        {todos.map(todo => (
-          <TodoRow
-            key={todo.id}
-            todo={todo}
-            onDelete={handleDeleteTodo}
-            onComplete={handleCompleteTodo}
+    <div className="flex justify-center items-center w-full h-full">
+      <div className="w-full max-w-md mx-auto mt-8 bg-gray-100 rounded-md p-5">
+        <h1 className="text-2xl font-bold mb-4">Todo</h1>
+        <div>
+          {todos.map(todo => (
+            <TodoRow
+              key={todo.id}
+              todo={todo}
+              onDelete={handleDeleteTodo}
+              onComplete={handleCompleteTodo}
+            />
+          ))}
+        </div>
+        <hr className="mt-5 text-zinc-600"/>
+        <div className="flex mt-2">
+          <input
+            type="text"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            className="flex-1 border border-gray-300 px-4 py-2 rounded-md mr-2"
+            placeholder="Enter a new todo"
           />
-        ))}
+          <Button onClick={handleAddTodo} variant="big">Add</Button>
+        </div>
       </div>
-      <hr className="mt-5 text-zinc-600"/>
-      <div className="flex mt-2">
-        <input
-          type="text"
-          value={inputText}
-          onChange={(e) => setInputText(e.target.value)}
-          className="flex-1 border border-gray-300 px-4 py-2 rounded-md mr-2"
-          placeholder="Enter a new todo"
-        />
-        <Button onClick={handleAddTodo} variant="big">Add Todo</Button>
-      </div>
-
     </div>
   );
 };
 
 export default TodoList;
-
 
 
 
