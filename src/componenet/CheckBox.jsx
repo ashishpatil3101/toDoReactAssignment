bcd9e84cbd60f416f36dadaffbd1932199442b3c
Vaibhav Patil <ashishpatil3101@gmail.com>
2024-05-10 09:20:32 +0530

    changed stylling of checkbox

--- src/componenet/CheckBox.jsx
+++ src/componenet/CheckBox.jsx
@@ -6,7 +6,7 @@ const Checkbox = ({ label, isChecked, onChange }) => {
                 type="checkbox"
                 checked={isChecked}
                 onChange={onChange}
-                className={isChecked ? "h-5 w-5  appearance-none  border rounded-full bg-green-500" : "h-5 w-5  appearance-none  border rounded-full"}
+                className={isChecked ? "h-5 w-5  appearance-none  border  rounded-full bg-green-500" : "h-5 w-5  appearance-none  border border-gray-400 rounded-full"}
             />
             <span className="ml-2 text-gray-700">{label}</span>
         </label>
